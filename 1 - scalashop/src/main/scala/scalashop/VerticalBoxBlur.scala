b8a03a5dd4208c866b8650d6e0c3c805f671755f
shaido987 <shaido987@hotmail.com>
2018-02-23 16:06:51 +0800

    Assignment 1 completed

---
+++ 1 - scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,73 @@
+package scalashop
+
+import org.scalameter._
+import common._
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each column, `blur` traverses the pixels by going from top to
+   *  bottom.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {
+	  x <- from until end
+	  y <- 0 until src.height
+	}{
+	  val c = boxBlurKernel(src, x, y, radius)
+	  dst.update(x, y, c)
+	}
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  columns.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val step = (src.width / numTasks.toDouble).ceil.toInt
+	val range = 0 until src.width by step
+	val intervals = range.zip(range.tail ++ List(src.width))
+	
+	val computations = for ((from, to) <- intervals) yield {
+	  task {
+	    blur(src, dst, from, to, radius)
+	  }
+	}
+	computations.foreach(_.join)
+  }
+}
